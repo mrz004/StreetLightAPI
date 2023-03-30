e75177e2407796909b9d539e16d8de503a6ef8eb
mrz004 <90879352+mrz004@users.noreply.github.com>
2023-03-30 13:57:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,6 @@ const express = require("express");
 const fs = require("fs");
 const filename = "data.json";
 const port = process.env.PORT ?? 3000;
-const { networkInterfaces } = require('os');
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-        if (net.family === familyV4Value && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-const ip = results["en0"][0];
 
 const app = express();
 const { key, data } = JSON.parse(
@@ -82,5 +63,5 @@ app.get("/set", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server Started on port ${ip}:${port}`);
+  console.log(`Server Started on port ${port}`);
 });
